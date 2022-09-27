8835380964f96e3f963ad9866019594398277642
Arthur Godoy <arthurgodoygomides@gmail.com>
2022-09-27 14:16:47 -0300

    text-wrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <div class="shadow-lg tasksContainer rounded">
         @foreach ($tasks as $task)
             <div class="task">
-                <div class="form-check rounded px-5 py-3 mb-0 text-break">
+                <div class="form-check rounded px-5 me-3 py-3 mb-0 text-break">
                     <form action="{{ route('update', ['id' => $task->id]) }}" method="POST">
                         @csrf
                         @method('PUT')
@@ -21,7 +21,7 @@
                                 name="{{ $task->id }}"
                                 onclick="event.preventDefault(); this.closest('form').submit();"
                             >
-                            <label style=" text-decoration: line-through; color:rgb(163, 163, 163)" class="form-check-label " for="flexRadioDefault1">
+                            <label style=" text-decoration: line-through; color:rgb(163, 163, 163)" class="form-check-label" for="flexRadioDefault1">
                                 {{ $task->content }}
                             </label>
                             @else
